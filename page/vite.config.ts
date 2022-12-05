e5cbe863077e8bb80b53d7016c920d27459bad21
Luis José Rodríguez <luijorod@gmail.com>
2022-12-05 12:44:37 -0400

    configure vite.config.ts to include base /epistatsrd/ and change outDir to build

--- page/vite.config.ts
+++ page/vite.config.ts
@@ -4,5 +4,9 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/epistatsrd/",
+  build: {
+    outDir: "build",
+  },
   plugins: [react(), dsv()],
 });
