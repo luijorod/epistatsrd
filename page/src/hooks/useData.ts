de95a2c1fcb490850e5a49838ba39bdf5b20822c
Luis José Rodríguez <luijorod@gmail.com>
2023-01-05 14:45:14 -0500

    Add colera, covid, dengue, leptospirosis, malaria and mmi datasets to useData hook

--- page/src/hooks/useData.ts
+++ page/src/hooks/useData.ts
@@ -1,10 +1,18 @@
 import { Dispatch, SetStateAction, useState } from "react";
 
-import covid from "../assets/covid_data.csv";
+import { datasetVars } from "../utils";
+
+import colera from "../assets/col.csv";
+import covid from "../assets/cov.csv";
+import dengue from "../assets/den.csv";
+import leptospirosis from "../assets/lep.csv";
+import malaria from "../assets/mal.csv";
+import mmi from "../assets/mmi.csv";
 
 interface UseData {
-  dataset: any;
   data: any;
+  dataset: any;
+  datasetVars: any;
   datasets: any[];
   setData: Dispatch<SetStateAction<any>>;
   setDatasets: Dispatch<SetStateAction<any>>;
@@ -12,17 +20,27 @@ interface UseData {
 
 function getDataset(dataset: string = "covid"): any {
   switch (dataset) {
+    case "colera":
+      return [colera, datasetVars.colera];
     case "covid":
-      return covid;
+      return [covid, datasetVars.covid];
+    case "dengue":
+      return [dengue, datasetVars.dengue];
+    case "leptospirosis":
+      return [leptospirosis, datasetVars.leptospirosis];
+    case "malaria":
+      return [malaria, datasetVars.malaria];
+    case "mmi":
+      return [mmi, datasetVars.mmi];
     default:
       break;
   }
 }
 
 export function useData(datasetString: string = "covid"): UseData {
-  const dataset = getDataset(datasetString);
+  const [dataset, datasetVars] = getDataset(datasetString);
   const [data, setData] = useState(dataset);
   const [datasets, setDatasets] = useState([dataset]);
 
-  return { dataset, data, datasets, setData, setDatasets };
+  return { data, dataset, datasetVars, datasets, setData, setDatasets };
 }
